6d9a0fbc90c9a646068db506f8ce42983899eae8
jimmyGoossens <jimmy.v.goossens@gmail.com>
2017-12-04 14:32:01 +0100

    commit pour commit

---
+++ app.js
@@ -0,0 +1,14 @@
+class Block{
+	
+constructor(index, timestamp, data ,prviousHash = ''){
+	
+
+	this.index = index;
+	this.timestamp = timestamp;
+	this.data = data;
+	this.previousHash = previousHash
+	this.hash = '';
+}
+
+calculateHash
+}
