57a7705f2ddfbcad6869a2bf5c380e4b1f37213c
jimmyGoossens <jimmy.v.goossens@gmail.com>
2017-12-27 15:15:40 +0100

    first part

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block{
 	
-constructor(index, timestamp, data ,prviousHash = ''){
-	
+	constructor(index, timestamp, data ,previousHash = ''){
+		
 
-	this.index = index;
-	this.timestamp = timestamp;
-	this.data = data;
-	this.previousHash = previousHash
-	this.hash = '';
+		this.index = index;
+		this.timestamp = timestamp;
+		this.data = data;
+		this.previousHash = previousHash
+		this.hash = this.calculateHash();
+	}
+
+	calculateHash(){
+
+	return SHA256(this.index +this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+
+	}
 }
 
-calculateHash
+class Blockchain{
+
+	constructor(){
+
+		this.chain = [this.createGenesisBlock()];
+
+
+	}
+	createGenesisBlock(){
+
+		return new Block(0, "01/01/2017", "Genesis block", "0")
+	}
+	getLatestBlock(){
+
+		return this.chain[this.chain.length -1];
+	}
+	addBlock(newBlock){
+
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+	}
+
 }
+
+let jamescoin = new Blockchain();
+jamescoin.addBlock(new Block(1,"10/03/2017",{amount:4} ));
+jamescoin.addBlock(new Block(2,"13/03/2017",{amount:5} ));
+
+console.log(JSON.stringify(jamescoin,null,4));
